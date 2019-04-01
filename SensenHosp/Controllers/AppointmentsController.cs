952550e8b3796e2e4b3d51945dd6820c18d98ef8
anjalipanchal1124 <anjalipanchal1124@gmail.com>
2019-04-01 11:23:32 -0400

    Add Update List of Appointments

--- SensenHosp/Controllers/AppointmentsController.cs
+++ SensenHosp/Controllers/AppointmentsController.cs
@@ -56,5 +56,79 @@ namespace SensenHosp.Controllers
             return View(_context.Appointments.ToList());
 
         }
+
+        //GET: Appointment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(400);
+            }
+            Appointment appointment = _context.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            return View(appointment);
+        }
+
+        //POST: Appointment/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id,string FirstName, string MiddleName, string LastName, string EmailId, string MobileNo, string Description, string DoctorName, DateTime AppointmentDate, string AppointmentTime)
+        {
+            if (id == null || (_context.Appointments.Find(id) == null))
+            {
+                return NotFound();
+            }
+            string query = "update appointments set FirstName = @firstname," +
+                "MiddleName = @middlename, " +
+                "LastName = @lastname, " +
+                "EmailId = @emailid, " +
+                "MobileNo = @mobileno, " +
+                "Description = @description, " +
+                "DoctorName = @doctorname, " +
+                "AppointmentDate = @appointmentdate, " +
+                "AppointmentTime = @appointmenttime " +
+                "where ID = @id";
+
+            SqlParameter[] myparams = new SqlParameter[10];
+
+            myparams[0] = new SqlParameter("@id", id);
+            myparams[1] = new SqlParameter("@firstname", FirstName);
+            myparams[2] = new SqlParameter("@middlename", MiddleName);
+            myparams[3] = new SqlParameter("@lastname", LastName);
+            myparams[4] = new SqlParameter("@emailid", EmailId);
+            myparams[5] = new SqlParameter("@mobileno", MobileNo);
+            myparams[6] = new SqlParameter("@description", Description);
+            myparams[7] = new SqlParameter("@doctorname", DoctorName);
+            myparams[8] = new SqlParameter("@appointmentdate", AppointmentDate);
+            myparams[9] = new SqlParameter("@appointmenttime", AppointmentTime);
+
+            _context.Database.ExecuteSqlCommand(query, myparams);
+            return RedirectToAction("Edit/"+id);
+        }
+        //GET: Appointment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(400);
+            }
+            Appointment appointment = _context.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            return View(appointment);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if((id ==null)||_context.Appointments.Find(id)==null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
     }
 }
