cbce1c1b3fb78f3e92f3d5b04587786a2596d997
Bibek <bibekmanshrestha@gmail.com>
2019-04-10 00:48:38 -0400

    add blog to home page

---
+++ SensenHosp/Models/ViewModels/HomePage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SensenHosp.Models.ViewModels
+{
+    public class HomePage
+    {
+        public HomePage()
+        {
+
+        }
+
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+        public IEnumerable<Testimonial> Testimonials { get; set; }
+    }
+
+}
