6b6b5200425c05e761aef8fe8f384de2774b4c9a
anjalipanchal1124 <anjalipanchal1124@gmail.com>
2019-04-12 16:56:24 -0400

    Edit, Delete and details of Review on Doctor

--- SensenHosp/Controllers/ReviewOnDoctorController.cs
+++ SensenHosp/Controllers/ReviewOnDoctorController.cs
@@ -33,7 +33,7 @@ namespace SensenHosp.Controllers
             {
                 _context.Add(review);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Create));
+                return RedirectToAction(nameof(List));
             }
             return View(review);
         }
@@ -41,6 +41,99 @@ namespace SensenHosp.Controllers
         public async Task<IActionResult> List()
         {
             return View(await _context.ReviewOnDoctor.ToListAsync());
+        }
+
+        //GET: ReviewOnDoctor/Edit/5
+        public async Task<IActionResult> Edit(int?id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(400);
+            }
+            ReviewOnDoctor review = _context.ReviewOnDoctor.Find(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            return View(review);
+        }
+        //POST: ReviewOnDoctor/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id,[Bind("ReviewId", "DoctorName", "Message", "Reply")] ReviewOnDoctor review)
+        {
+            if(id != review.ReviewId)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewOnDoctorExists(review.ReviewId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(List));
+            }
+            return View(review);
+        }
+
+        //GET: ReviewOnDoctor/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(400);
+            }
+            ReviewOnDoctor review = _context.ReviewOnDoctor.Find(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            return View(review);
+        }
+
+        //GET: ReviewOnDoctor/Delete/1
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.ReviewOnDoctor.SingleOrDefaultAsync(m => m.ReviewId == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            return View(review);
+        }
+
+        //POST: ReviewOnDoctor/Delete/1
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var review = await _context.ReviewOnDoctor.SingleOrDefaultAsync(m => m.ReviewId == id);
+            _context.ReviewOnDoctor.Remove(review);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+
+        }
+
+        private bool ReviewOnDoctorExists(int id)
+        {
+            return _context.ReviewOnDoctor.Any(e => e.ReviewId == id);
         }
     }
 }
